dffa569d4c17e759b90332d0b40978d11c80ae95
woojin2296 <woojin2296@kakao.com>
2025-05-20 18:23:45 +0900

    init be

---
+++ plavo-be/src/main/java/me/noplanb/plavobe/PlavoBeApplication.java
@@ -0,0 +1,13 @@
+package me.noplanb.plavobe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlavoBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlavoBeApplication.class, args);
+    }
+
+}
---
+++ plavo-be/src/test/java/me/noplanb/plavobe/PlavoBeApplicationTests.java
@@ -0,0 +1,13 @@
+package me.noplanb.plavobe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlavoBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
